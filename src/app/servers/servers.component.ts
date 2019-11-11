cf9cc8725c22f5f461e1208cd5f1996613ee26ec
DESKTOP-O16UD8S\Carter Liu <gtk.carter.liu@gmail.com>
2019-11-11 21:54:32 +0800

    add servers component

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was Created!';
+  serverName = 'initalName';
+  constructor() {
+    setTimeout(() =>{
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server Was Created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
+
+}
